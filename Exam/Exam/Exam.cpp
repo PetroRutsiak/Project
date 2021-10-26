9573bb7aa3257d3a671e6cec78d6a2364a05278d
DESKTOP-UAAA3E1\petro <petrorutsyak@gmail.com>
2021-10-26 13:15:13 +0300

    trial

--- Exam/Exam/Exam.cpp
+++ Exam/Exam/Exam.cpp
@@ -1,20 +1,105 @@
-﻿// Exam.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <list>
+enum HeroType
+{
+    Goblin,
+    Orc,
+    Dragon,
+    Ogre,
+    Skeleton,
+    Troll,
+    Vempire,
+    Zombie,
+    Bane,
+    Basilisk,
+    Beelzebub,
+    Bigfoot,
+    Boogeyman,
+    Centaur,
+    Chimera,
+    Chuckie
+};
+std::string getTypeString(HeroType m_type)
+{
+    switch (m_type)
+    {
+    case Goblin:return "Goblin";
+    case Orc: return "Orc";
+    case Dragon: return "Dragon";
+    case Ogre: return "Ogre";
+    case Skeleton: return "Skeleton";
+    case Troll: return "Troll";
+    case Vempire: return "Vempire";
+    case Zombie: return "Zombie";
+    case Bane:return "Bane";
+    case Basilisk:return "Basilisk";
+    case Beelzebub:return "Beelzebub";
+    case Bigfoot:return "Bigfoot";
+    case Boogeyman:return "Boogeyman";
+    case Centaur:return "Centaur";
+    case Chimera:return "Chimera";
+    case Chuckie:return "Chuckie";
+    }
+    return"Error!";
+}
 
-#include <iostream>
+class Hero
+{
+
+    int id;
+    std::string name;
+    int health;
+    int damage;
+public:
+    void setId(int newid)
+    {
+        id = newid;
+    }
+    void setName(std::string newname)
+    {
+        name = newname;
+    }
+    void setHP(int newhealth)
+    {
+        health = newhealth;
+    }
+    void setDamage(int newdamage)
+    {
+        damage = newdamage;
+    }
+    Hero (int id = 3228,int damage = 30, std::string name ="Dragon")
+    {
+        this->id = id;
+        this->damage = damage;
+        this->name = name;
+        this->health = health;
+    }
+    std::string getName()
+    {
+        return name;
+    }
+    int getID()
+    {
+        return id;
+    }
+    int getHealth()
+    {
+        return health;
+    }
+    int getDamage()
+    {
+        return damage;
+    }
+    void Print()
+    {
+        std::cout << this->getName() << " " << this->getID() << " " << this->getHealth() << "" << this->getDamage();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Hero* Dragon = new Hero(10,20,"Dragon");
+    Dragon->Print();
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
