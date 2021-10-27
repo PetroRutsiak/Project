b2e4c17c72115543153566bb643eb974d8d190c3
DESKTOP-UAAA3E1\petro <petrorutsyak@gmail.com>
2021-10-27 12:37:11 +0300

    Managers

--- Exam/Exam/Exam.cpp
+++ Exam/Exam/Exam.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <string>
 #include <list>
-
+#include <ctime>
+#include <chrono>
+#include <thread>
 enum HeroType
 {
     Goblin,
@@ -44,31 +46,17 @@ std::string getTypeString(HeroType m_type)
     }
     return"Error!";
 }
-
 class Hero
 {
-
     int id;
     std::string name;
     int health;
     int damage;
 public:
-    void setId(int newid)
-    {
-        id = newid;
-    }
-    void setName(std::string newname)
-    {
-        name = newname;
-    }
-    void setHP(int newhealth)
-    {
-        health = newhealth;
-    }
-    void setDamage(int newdamage)
-    {
-        damage = newdamage;
-    }
+    void setId(int newid) { id = newid; }
+    void setName(std::string newname) { name = newname; }
+    void setHP(int newhealth) { health = newhealth; }
+    void setDamage(int newdamage) { damage = newdamage; }
     Hero (int id,int damage, int health,  std::string name)
     {
         this->id = id;
@@ -76,22 +64,11 @@ public:
         this->name = name;
         this->health = health;
     }
-    std::string getName()
-    {
-        return name;
-    }
-    int getID()
-    {
-        return id;
-    }
-    int getHealth()
-    {
-        return health;
-    }
-    int getDamage()
-    {
-        return damage;
-    }
+    std::string getName() { return name;}
+    int getID() { return id; }
+    int getHealth() { return health; }
+    int getDamage() { return damage; }
+    Hero(){ }
     void Print()
     {
         std::cout << this->getName() << " " << this->getID() << " " << this->getDamage() << " " << this->getHealth();
@@ -102,40 +79,118 @@ class Player
     int id;
     int rank;
     std::string name;
-
 public:
-    int getID()
-    {
-        return id;
-    }
-    void setRank(int newrank)
-    {
-        rank = newrank;
-    }
-    int getRank()
-    {
-        return rank;
-    }
-    std::string getName()
-    {
-        return name;
-    }
+    int getID() { return id; }
+    void setRank(int newrank) { rank = newrank; }
+    int getRank() { return rank; }
+    std::string getName() { return name; }
     Player(int id, int rank, std::string name)
     {
         this->id = id;
         this->rank = rank;
         this->name = name;
     }
+    Player() { }
     void Print()
     {
         std::cout << this->getName() << " " << this->getID() << " " << this->getRank();
     }
-
+};
+class Team
+{
+public:
+    std::string team_name;
+    Player  player_list[5];
+    Hero hero_list[5];
+    Team(std::string team_name, Player player_list[5], Hero hero_list[5])
+    {
+        this->team_name = team_name;
+        for (int i = 0; i < 5; i++)
+        {
+            this->player_list[i] = player_list[i];
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            this->hero_list[i] = hero_list[i];
+        }
+    }
+};
+class PlayerManager
+{
+public:
+    Player CreatePlayer()
+    {
+        srand (time (NULL));
+        std::this_thread::sleep_for(std::chrono::milliseconds(900));
+        int randomid = rand() % 100;
+        int randomrank = rand() % 100;
+        Player player(randomid, randomrank, " Sasha ");   
+        return player;
+    }
+    void ShowPlayerInfo(Player player)
+    {
+        player.Print();
+    }
+};
+class HeroManager
+{
+public:
+    Hero CreateHero()
+    {
+        srand(time(NULL));
+        std::this_thread::sleep_for(std::chrono::milliseconds(900));
+        int randomid = rand() % 100;
+        int randomHP = rand() % 100;
+        int randomDamage = rand() % 100;
+        Hero hero(randomid, randomHP, randomDamage, "dora");
+        return hero;
+    }
+    void ShowInfo(Hero hero)
+    {
+        hero.Print();
+    }
+};
+class TeamManager
+{
+public:
+    Team  GenerateNewTeam()
+    {
+        HeroManager heromanager;
+        PlayerManager playermanager;
+        Player player_list[5];
+        Hero hero_list[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Hero Hero = heromanager.CreateHero();
+            hero_list[i] = Hero;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Player Player = playermanager.CreatePlayer();
+            player_list[i] = Player;
+        }
+        Team first(" Radiant ", player_list, hero_list);
+        return first;
+    }
+    void GetTeamInfo(Team first)
+    {
+        std::cout << first.team_name << "\n";
+        for (int i = 0; i < 5; i++)
+        {
+            first.hero_list[i].Print();
+            std::cout << std::endl;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            first.player_list[i].Print();
+            std::cout << std::endl;
+        }
+    }
 };
 int main()
 {
-    Hero* Dragon = new Hero(786, 70, 100, "Centaur");
-    Player* Dima = new Player(3, 7, " DimonTheSlayer");
-    Dima->Print();
+    TeamManager Dire;
+    Team Radiant = Dire.GenerateNewTeam();
+    Dire.GetTeamInfo(Radiant);
     return 0;
 }
