96a542a239bb679963b9fb04ea3b7fa111235e59
DESKTOP-UAAA3E1\petro <petrorutsyak@gmail.com>
2021-11-14 13:13:42 +0200

    classes.h

--- Exam/Exam/Exam.cpp
+++ Exam/Exam/Exam.cpp
@@ -64,64 +64,7 @@ std::string getPlayerName(int number)
     return"Error!";
 }
 
-class Hero
-{
-    int ID;
-    std::string Name;
-    int Health;
-    int Damage;
-public:
-    int GetId() { return ID; }
-    std::string GetName() { return Name; }
-    int GetHP() { return Health; }
-    int GetDamage() { return Damage; }
 
-    int setHP(int hp)
-    {
-        this->Health = hp;
-    }
-
-    Hero(int id, std::string name, int hp, int damage)
-        :ID(ID), Name(name), Health(hp), Damage(damage) {}
-    Hero() {}
-};
-class Player
-{
-    int ID;
-    std::string Name;
-    int Rank;
-public:
-    int GetId() { return ID; }
-    std::string GetName() { return Name; }
-    int GetRank() { return Rank; }
-
-    void SetRank(int newRank) {
-        if (newRank > 0)
-            Rank = newRank;
-        else Rank = 0;
-    }
-
-
-    Player(int id, std::string name, int rank)
-        :ID(id), Name(name), Rank(rank) {}
-    Player() {}
-};
-class Team
-{
-public:
-    std::string Name;
-    Hero HeroList[5];
-    Player PlayerList[5];
-    Team(std::string name) :Name(name) {}
-    Team(std::string name, Hero heroList[5], Player playerList[5]) :Name(name) {
-        for (int i = 0; i < 5; i++)
-        {
-            HeroList[i] = heroList[i];
-            PlayerList[i] = playerList[i];
-        }
-    }
-    Team() {}
-};
 class PlayerManager
 {
 public:
